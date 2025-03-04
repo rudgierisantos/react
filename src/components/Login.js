60490e0ac4b023a670713896c0853f14b1425f30
Rudgieri Santos <rudgieri.santos@hotmail.com>
2025-03-04 15:54:33 -0300

    Melhoria login

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,8 @@ function Login() {
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
-    const handleLogin = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
         if (username === 'admin' && password === 'admin') {
             navigate(RotasUtil.dashboard.path);
         } else {
@@ -20,21 +21,23 @@ function Login() {
     };
 
     return (
-        <div style={{ textAlign: 'center' }} className="flex justify-center items-center h-screen bg-gray-100">
-            <Card className="w-96 p-5 shadow-lg">
-                <h2 className="text-center text-xl mb-4">Login</h2>
-                <label>Usuário</label>
-                <div className="mb-3">
-                    <InputText className="w-full" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </div>
+        <form onSubmit={handleLogin}>
+            <div style={{ textAlign: 'center' }} className="flex justify-center items-center h-screen bg-gray-100">
+                <Card className="w-96 p-5 shadow-lg">
+                    <h2 className="text-center text-xl mb-4">Login</h2>
+                    <label>Usuário</label>
+                    <div className="mb-3">
+                        <InputText className="w-full" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                    </div>
 
-                <label>Senha</label>
-                <div className="mb-3">
-                    <Password className="w-full" feedback={false} value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <Button style={{ marginTop: '10px' }} label="Entrar" icon="pi pi-sign-in" className="w-full" onClick={handleLogin} />
-            </Card>
-        </div>
+                    <label>Senha</label>
+                    <div className="mb-3">
+                        <Password className="w-full" feedback={false} value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    </div>
+                    <Button type="submit" style={{ marginTop: '10px' }} label="Entrar" icon="pi pi-sign-in" className="w-full" />
+                </Card>
+            </div>
+        </form>
     );
 }
 
